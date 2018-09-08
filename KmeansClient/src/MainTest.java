2294c1fc73139c4b65b9fa0e9023316d1f6d28a1
Ros290 <redrum290@gmail.com>
2018-09-08 10:06:22 +0200

    Interfaccia Client in costruzione

--- KmeansClient/src/MainTest.java
+++ KmeansClient/src/MainTest.java
@@ -1,3 +1,4 @@
+import javax.swing.JApplet;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -8,10 +9,20 @@ import java.net.SocketException;
 
 
 import keyboardinput.Keyboard;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
 
 
+public class MainTest extends JApplet {
 
-public class MainTest {
 
 	/**
 	 * @param args
@@ -19,21 +30,25 @@ public class MainTest {
 	private static String DEFAULT_HOST = "localhost";
 	private static int DEFAULT_PORT = 8080;
 	Socket socket;
-
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	
 	public MainTest(String ip, int port) throws IOException{
 		InetAddress addr = InetAddress.getByName(ip); //ip
 		System.out.println("addr = " + addr);
 		socket = new Socket(addr, port); //Port
 		System.out.println(socket);
 	}
-
-	/**
-	 * Si occupa della ricezione sicura di un oggetto via socket
-	 * @param socket la quale ricevere l'oggetto
-	 * @return oggetto ricevuto dal socket specificato
-	 * @throws ClassNotFoundException nel caso la classe ricevuta non vi è riconosciuta
+	
+    /**
+     * Si occupa della ricezione sicura di un oggetto via socket
+     * @param socket la quale ricevere l'oggetto
+     * @return oggetto ricevuto dal socket specificato
+     * @throws ClassNotFoundException nel caso la classe ricevuta non vi è riconosciuta
 	 * @throws IOException nel caso si verifichi un errore in fase di lettura
-	 */
+     */
 	private static Object readObject(Socket socket) throws ClassNotFoundException, IOException {
 		Object o;
 		ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
@@ -41,8 +56,8 @@ public class MainTest {
 		return o;
 	}
 	/**
-	 * Si occupa dell'invio sicuro di un oggetto via socket
-	 * @param socket la quale riceverà l'oggetto
+     * Si occupa dell'invio sicuro di un oggetto via socket
+     * @param socket la quale riceverà l'oggetto
 	 * @param o oggetto da inviare
 	 * @throws IOException nel caso si verifichi un errore in fase di scrittura
 	 */
@@ -51,21 +66,20 @@ public class MainTest {
 		out.writeObject(o);
 		out.flush();
 	}
-
+	
 	private int menu(){
 		int answer;
 		System.out.println("Scegli una opzione");
 		do{
 			System.out.println("(1) Carica Cluster da File");
 			System.out.println("(2) Carica Dati");
-			System.out.println("(0) Termina Applicazione");
 			System.out.print("Risposta:");
 			answer=Keyboard.readInt();
 		}
-		while(answer<0 || answer>2);
+		while(answer<=0 || answer>2);
 		return answer;
 	}
-
+	
 	/**
 	 * Ricava i cluster all'interno di uno specifico file, definito dall'utente, indicando inoltre
 	 * la tabella da cui reperire la collezione di dati a cui è riferita.
@@ -88,7 +102,7 @@ public class MainTest {
 			return (String)readObject(socket);
 		else throw new ServerException(result);
 	}
-
+	
 	/**
 	 * Ricava la collezione di dati, su cui eseguire il calcolo dei cluster, tramite il db
 	 * @throws SocketException
@@ -105,7 +119,7 @@ public class MainTest {
 		if(!result.equals("OK"))
 			throw new ServerException(result);
 	}
-
+	
 	/**
 	 * Effettua il calcolo dei cluster sulla collezione di dati ricavata dal server
 	 * @return stampa dei cluster rilevati
@@ -126,7 +140,7 @@ public class MainTest {
 		}
 		else throw new ServerException(result);
 	}
-
+	
 	/**
 	 * Deposita il cluster all'interno di un file. Tale file sarà depositato su lato Server
 	 * @throws SocketException
@@ -141,13 +155,9 @@ public class MainTest {
 		writeObject(socket,fileName);
 		String result = (String)readObject(socket);
 		if(!result.equals("OK"))
-			throw new ServerException(result);
+			 throw new ServerException(result);
 	}
 	
-	private void closeSocket() throws IOException{
-		writeObject(socket,4);
-	}
-
 	public static void main(String[] args){
 		String ip;
 		int port;
@@ -174,43 +184,19 @@ public class MainTest {
 		do{
 			int menuAnswer=main.menu();
 			switch(menuAnswer){
-			case 1:
-				try {
-					String kmeans=main.learningFromFile();
-					System.out.println(kmeans);
-				}
-				catch (SocketException e) {
-					System.err.println(e);
-					return;
-				}
-				catch (FileNotFoundException e) {
-					System.err.println(e);
-					return ;
-				} catch (IOException e) {
-					System.err.println(e);
-					return;
-				} catch (ClassNotFoundException e) {
-					System.err.println(e);
-					return;
-				}
-				catch (ServerException e) {
-					System.err.println(e.getMessage());
-				}
-				break;
-			case 2: // learning from db
-
-				answer='y';//itera per learning al variare di k
-				while(true){
-					try{
-						main.storeTableFromDb();
-						break; //esce fuori dal while
+				case 1:
+					try {
+						String kmeans=main.learningFromFile();
+						System.out.println(kmeans);
 					}
-
 					catch (SocketException e) {
 						System.err.println(e);
 						return;
 					}
-					catch (IOException e) {
+					catch (FileNotFoundException e) {
+						System.err.println(e);
+						return ;
+					} catch (IOException e) {
 						System.err.println(e);
 						return;
 					} catch (ClassNotFoundException e) {
@@ -219,60 +205,145 @@ public class MainTest {
 					}
 					catch (ServerException e) {
 						System.err.println(e.getMessage());
-						answer = 'n';
-						break;
 					}
-				} //end while [viene fuori dal while con un db (in alternativa il programma termina)
-
-				while(answer == 'y'){
-					try{
-						String clusterSet=main.learningFromDbTable();
-						System.out.println(clusterSet);
-
-						main.storeClusterInFile();
-
+					break;
+				case 2: // learning from db
+				
+					answer='y';//itera per learning al variare di k
+					while(true){
+						try{
+							main.storeTableFromDb();
+							break; //esce fuori dal while
+						}
+						
+						catch (SocketException e) {
+							System.err.println(e);
+							return;
+						}
+						catch (IOException e) {
+							System.err.println(e);
+							return;
+						} catch (ClassNotFoundException e) {
+							System.err.println(e);
+							return;
+						}
+						catch (ServerException e) {
+							System.err.println(e.getMessage());
+							answer = 'n';
+							break;
+						}
+					} //end while [viene fuori dal while con un db (in alternativa il programma termina)
+						
+					while(answer == 'y'){
+						try{
+							String clusterSet=main.learningFromDbTable();
+							System.out.println(clusterSet);
+							
+							main.storeClusterInFile();
+									
+						}
+						catch (SocketException e) {
+							System.err.println(e);
+							return;
+						}
+						catch (FileNotFoundException e) {
+							System.err.println(e);
+							return;
+						} 
+						catch (ClassNotFoundException e) {
+							System.err.println(e);
+							return;
+						}catch (IOException e) {
+							System.err.println(e);
+							return;
+						}
+						catch (ServerException e) {
+							System.err.println(e.getMessage());
+						}
+						System.out.print("Vuoi ripetere l'esecuzione?(y/n)");
+						answer=Keyboard.readChar();
 					}
-					catch (SocketException e) {
-						System.err.println(e);
-						return;
-					}
-					catch (FileNotFoundException e) {
-						System.err.println(e);
-						return;
-					} 
-					catch (ClassNotFoundException e) {
-						System.err.println(e);
-						return;
-					}catch (IOException e) {
-						System.err.println(e);
-						return;
-					}
-					catch (ServerException e) {
-						System.err.println(e.getMessage());
-					}
-					System.out.print("Vuoi ripetere l'esecuzione?(y/n)");
-					answer=Keyboard.readChar();
-				}
-				break; //fine case 2
-			case 0:
-				try {
-					main.closeSocket();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				return;
-			default:
-				System.out.println("Opzione non valida!");
+					break; //fine case 2
+					default:
+					System.out.println("Opzione non valida!");
 			}
-
+			
 			System.out.print("Vuoi scegliere una nuova operazione da menu?(y/n)");
 			if(Keyboard.readChar()!='y')
 				break;
-		}
+			}
 		while(true);
+		}
+
+	/**
+	 * Create the applet.
+	 */
+	public MainTest() {
+		getContentPane().setLayout(null);
+		
+		JTabbedPane JTabbedPane = new JTabbedPane();
+		JTabbedPane.setBounds(10, 11, 430, 278);
+		getContentPane().add(JTabbedPane);
+		
+		JPanel DBPanel = new JPanel();
+		JTabbedPane.addTab("DB", null, DBPanel, null);
+		DBPanel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(81, 11, 86, 20);
+		DBPanel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(254, 11, 86, 20);
+		DBPanel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblTable = new JLabel("Table:");
+		lblTable.setBounds(22, 14, 46, 14);
+		DBPanel.add(lblTable);
+		
+		JLabel lblK = new JLabel("k:");
+		lblK.setBounds(226, 14, 18, 14);
+		DBPanel.add(lblK);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(10, 117, 405, 91);
+		DBPanel.add(textPane);
+		
+		JButton btnMine = new JButton("MINE");
+		btnMine.setBounds(155, 219, 89, 23);
+		DBPanel.add(btnMine);
+		
+		JPanel FilePanel = new JPanel();
+		FilePanel.setLayout(null);
+		JTabbedPane.addTab("FILE", null, FilePanel, null);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(81, 11, 86, 20);
+		FilePanel.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(254, 11, 86, 20);
+		FilePanel.add(textField_3);
+		
+		JLabel label = new JLabel("Table:");
+		label.setBounds(22, 14, 46, 14);
+		FilePanel.add(label);
+		
+		JLabel label_1 = new JLabel("k:");
+		label_1.setBounds(226, 14, 18, 14);
+		FilePanel.add(label_1);
+		
+		JTextPane textPane_1 = new JTextPane();
+		textPane_1.setBounds(10, 117, 405, 91);
+		FilePanel.add(textPane_1);
+		
+		JButton btnStoreFromFile = new JButton("STORE FROM FILE");
+		btnStoreFromFile.setBounds(155, 219, 121, 23);
+		FilePanel.add(btnStoreFromFile);
+
 	}
 }
-
-
-
